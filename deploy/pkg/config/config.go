ee1b43977d5c65856f302ed9cb442dec649acf1b
Takafumi Iju <ijufumi@gmail.com>
2022-10-01 11:11:49 +0900

    Add code for deploy

---
+++ deploy/pkg/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+)
+
+// Config is application configuration
+type Config struct {
+	AwsAccessKeyID    string `env:"AWS_ACCESS_KEY_ID"`
+	AwsRegion         string `env:"AWS_REGION"`
+	CdkDefaultAccount string `env:"CDK_DEFAULT_ACCOUNT"`
+	CdkDefaultRegion  string `env:"CDK_DEFAULT_REGION"`
+
+	Vpc struct {
+		Name      string `env:"VPC_NAME"`
+		CidrBlock string `env:"CIDR_BLOCK"`
+	}
+	Cluster struct {
+		Name  string `env:"CLUSTER_NAME"`
+		Image struct {
+			RepositoryName string `env:"CLUSTER_IMAGE_REPOSITORY_NAME"`
+			Tag            string `env:"CLUSTER_IMAGE_TAG"`
+		}
+	}
+	LoadBalancer struct {
+		DomainName     string `env:"LOAD_BALANCER_DOMAIN_NAME"`
+		CertificateArn string `env:"LOAD_BALANCER_CERTIFICATE_ARN"`
+	}
+	Route53 struct {
+		DomainName string `env:"ROUTE53_DOMAIN_NAME"`
+	}
+}
+
+// Load returns configuration made from environment variables
+func Load() *Config {
+	_ = godotenv.Load()
+	c := Config{}
+	_ = env.Parse(&c)
+
+	return &c
+}
---
+++ deploy/pkg/stacks/vpc.go
@@ -0,0 +1,37 @@
+package stacks
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/ijufumi/eks-deploy-sample/deploy/pkg/config"
+)
+
+func CreateVPC(scope constructs.Construct) awsec2.Vpc {
+	configuration := config.Load()
+	vpcID := fmt.Sprintf("id-%s", configuration.Vpc.Name)
+	subnetConfigurations := []*awsec2.SubnetConfiguration{
+		{
+			Name:       jsii.String(fmt.Sprintf("public-subnet-%s", configuration.Vpc.Name)),
+			SubnetType: awsec2.SubnetType_PUBLIC,
+		},
+		{
+			Name:       jsii.String(fmt.Sprintf("private1-subnet-%s", configuration.Vpc.Name)),
+			SubnetType: awsec2.SubnetType_PRIVATE_WITH_NAT,
+		},
+		{
+			Name:       jsii.String(fmt.Sprintf("private2-subnet-%s", configuration.Vpc.Name)),
+			SubnetType: awsec2.SubnetType_PRIVATE_ISOLATED,
+		},
+	}
+	props := awsec2.VpcProps{
+		VpcName:             jsii.String(configuration.Vpc.Name),
+		Cidr:                jsii.String(configuration.Vpc.CidrBlock),
+		MaxAzs:              jsii.Number(2),
+		SubnetConfiguration: &subnetConfigurations,
+	}
+
+	return awsec2.NewVpc(scope, jsii.String(vpcID), &props)
+}
