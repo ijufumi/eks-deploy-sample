60e66b0fe59acb4e7670ef210cb11cdbd866fcf9
Takafumi Iju <ijufumi@gmail.com>
2022-12-07 07:04:47 +0900

    Change cluster type

--- deployments/pkg/stacks/eks.go
+++ deployments/pkg/stacks/eks.go
@@ -1,26 +1,35 @@
 package stacks
 
 import (
-	"fmt"
-
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awseks"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+
+	//	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/ijufumi/eks-deploy-sample/deployments/pkg/config"
 )
 
-func CreateEKS(scope constructs.Construct, configuration config.Config) {
-	eksTaskRole := awsiam.NewRole(scope, jsii.String("eks-task-role"), &awsiam.RoleProps{
-		AssumedBy: awsiam.NewServicePrincipal(jsii.String("eks.amazonaws.com"), &awsiam.ServicePrincipalOpts{}),
-	})
+func CreateEKS(scope constructs.Construct, configuration config.Config, vpc awsec2.Vpc) awseks.Cluster {
+	// eksTaskRole := awsiam.NewRole(scope, jsii.String("eks-task-role"), &awsiam.RoleProps{
+	// 	AssumedBy: awsiam.NewServicePrincipal(jsii.String("eks.amazonaws.com"), &awsiam.ServicePrincipalOpts{}),
+	// })
 
-	id := fmt.Sprintf("id-%s", configuration.Cluster.Name)
-	props := awseks.ClusterProps{
+	subnets := []*awsec2.SubnetSelection{
+		{
+			SubnetType: awsec2.SubnetType_PRIVATE_WITH_NAT,
+		},
+	}
+	id := "eks-cluster-id"
+	props := awseks.FargateClusterProps{
 		Version:     awseks.KubernetesVersion_Of(jsii.String(configuration.Cluster.K8SVersion)),
 		ClusterName: jsii.String(configuration.Cluster.Name),
-		Role:        eksTaskRole,
+		// Role:        eksTaskRole,
+		Vpc:        vpc,
+		VpcSubnets: &subnets,
 	}
 
-	cluster := awseks.NewCluster(scope, jsii.String(id), &props)
+	cluster := awseks.NewFargateCluster(scope, jsii.String(id), &props)
+
+	return cluster
 }
