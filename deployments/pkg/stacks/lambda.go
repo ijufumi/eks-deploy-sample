fc081a121c236259f521be32fe1fa579b0887843
Takafumi Iju <ijufumi@gmail.com>
2022-12-10 07:26:36 +0900

    [WIP] Add lambda

---
+++ deployments/pkg/stacks/lambda.go
@@ -0,0 +1,31 @@
+package stacks
+
+import (
+	"os"
+	"path"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecrassets"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/ijufumi/eks-deploy-sample/deployments/pkg/configs"
+)
+
+func CreateLambda(scope constructs.Construct, config *configs.Config) awslambda.DockerImageFunction {
+	current, _ := os.Getwd()
+	imageProps := &awslambda.AssetImageCodeProps{
+		Platform: awsecrassets.Platform_LINUX_AMD64(),
+	}
+
+	props := &awslambda.DockerImageFunctionProps{
+		Code: awslambda.DockerImageCode_FromImageAsset(
+			jsii.String(path.Join(current, config.Lambda.Image.File)),
+			imageProps,
+		),
+	}
+
+	id := jsii.String("id-lambda")
+	lambdaFunction := awslambda.NewDockerImageFunction(scope, id, props)
+
+	return lambdaFunction
+}
