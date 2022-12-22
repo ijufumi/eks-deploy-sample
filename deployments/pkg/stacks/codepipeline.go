f12ce8dd871da41581d4f5964c5d6614dc582c40
Takafumi Iju <ijufumi@gmail.com>
2022-12-23 08:33:07 +0900

    Delete assumerole

--- deployments/pkg/stacks/codepipeline.go
+++ deployments/pkg/stacks/codepipeline.go
@@ -46,10 +46,6 @@ func CreateCodepipeline(scope constructs.Construct, config *configs.Config, buck
 		Actions:   jsii.Strings("eks:DescribeCluster"),
 		Resources: jsii.Strings(*cluster.ClusterArn()),
 	}))
-	buildRole.AddToPolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
-		Actions:   jsii.Strings("sts:AssumeRole"),
-		Resources: jsii.Strings("*"),
-	}))
 	buildProject := build.NewPipelineProject(scope, jsii.String("id-codebuild"), &build.PipelineProjectProps{
 		Role: buildRole,
 		Environment: &build.BuildEnvironment{
